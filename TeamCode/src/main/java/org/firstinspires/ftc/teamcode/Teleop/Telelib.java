72026a520949e3b3d90d1ec4f2b6d2c8a50ffd08
met4jet <phoenixheath@gmail.com>
2023-08-04 15:38:16 -0500

    Phoenix's coding tasks 8/4  3:34 pm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Telelib.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Telelib.java
@@ -1,24 +1,30 @@
 package org.firstinspires.ftc.teamcode.Teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public abstract class Telelib extends OpMode {
 
     // Difficulty: EASY
     // All: Create your motors and servos
+    public DcMotor motorLift;
+    public Servo clawServo;
 
     public void init(){
         // Difficulty: EASY
         // All: Hardware map your motors and servos
-
+        motorLift = hardwareMap.get(DcMotor.class, "lift_motor");
+        clawServo = hardwareMap.get(Servo.class, "claw_servo");
 
         // Difficulty: EASY
         // All: Set your motors' zero power behavior
-
+        motorLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         // Difficulty: EASY
         // All: Set your motors' directions
-
+        motorLift.setDirection(DcMotorSimple.Direction.FORWARD);
     }
     public void arcadeDrive(){
         // Difficulty: MEDIUM
@@ -30,6 +36,12 @@ public abstract class Telelib extends OpMode {
         // Difficulty: MEDIUM
         // Phoenix
         // Assume we are using the same button to open and close the claw
+        double currentPos = clawServo.getPosition(); // var used to track the state of the claw
+        if (gamepad1.a && currentPos == 0.0) {
+            clawServo.setPosition(1.0); // fully open
+        } else {
+            clawServo.setPosition(0.0); // fully closed
+        }
     }
 
     public void lift(){
@@ -37,6 +49,8 @@ public abstract class Telelib extends OpMode {
         // Phoenix
         // Assume we are using one motor to power the lift
         // Assume we are using the joysticks to control the lift
+        double liftPower = gamepad1.right_stick_y; // uses the right joystick to control the lift
+        motorLift.setPower(liftPower);
     }
 
     public void kill(){
