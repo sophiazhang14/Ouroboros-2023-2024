145dcf67485015c946471b2b3255b503e88c82c8
DarkSkyVoid <139508016+DarkSkyVoid@users.noreply.github.com>
2023-08-08 11:09:31 -0500

    This code that is being shown is the code for an arcade drive. I am sending you this because this is the assignment that you gave and If you could take a look at the code and tell me what I did wrong and help me with what is wrong if you could. Thank you and have a great day!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Claw.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Claw {
 
@@ -8,22 +9,26 @@ public class Claw {
 
     // Difficulty: EASY
     // Elin: Create your servo
+    public Servo claw;
 
     public Claw(LinearOpMode opMode){
         this.opMode = opMode;
 
         // Difficulty: EASY
         // ELin: Hardware Map servo
+        claw = opMode.hardwareMap.servo.get("claw");
 
     }
 
     public void openClaw(){
         // Difficulty: EASY
         // Elin
+        claw.setPosition(0);
     }
 
     public void closeClaw(){
         // Difficulty: EASY
         // ELin
+        claw.setPosition(1);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Telelib.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Telelib.java
@@ -9,39 +9,61 @@ public abstract class Telelib extends OpMode {
 
     // Difficulty: EASY
     // All: Create your motors and servos
-    public DcMotor motorLift;
-    public Servo clawServo;
+    public DcMotor fl;
+    public DcMotor fr;
+    public DcMotor bl;
+    public DcMotor br;
+    public Servo lift;
 
     public void init(){
         // Difficulty: EASY
         // All: Hardware map your motors and servos
-        motorLift = hardwareMap.get(DcMotor.class, "motorLift");
-        clawServo = hardwareMap.get(Servo.class, "clawServo");
+        fl = hardwareMap.get(DcMotor.class, "fl");
+        fr = hardwareMap.get(DcMotor.class, "fr");
+        bl = hardwareMap.get(DcMotor.class, "bl");
+        br = hardwareMap.get(DcMotor.class, "br");
+        lift = hardwareMap.get(Servo.class, "lift");
 
         // Difficulty: EASY
         // All: Set your motors' zero power behavior
-        motorLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         // Difficulty: EASY
         // All: Set your motors' directions
-        motorLift.setDirection(DcMotorSimple.Direction.FORWARD);
+        fl.setDirection(DcMotorSimple.Direction.FORWARD);
+        fr.setDirection(DcMotorSimple.Direction.REVERSE);
+        bl.setDirection(DcMotorSimple.Direction.FORWARD);
+        br.setDirection(DcMotorSimple.Direction.REVERSE);
     }
     public void arcadeDrive(){
         // Difficulty: MEDIUM
         // Elin
         // Assume we are using a four motor mecanum drive train
+        double vertical; // this is a variable
+        double horizontal; // this is a variable
+        double pivot; // this is a variable
+
+        // The variables are sent to an important joysticks on gamepad1
+        vertical = gamepad1.left_stick_y;
+        horizontal = gamepad1.left_stick_x;
+        pivot = gamepad1.right_stick_x;
+
+        // These are equations that help move the robot forward, backwards, left, and right
+        fl.setPower(pivot + (vertical - horizontal));
+        fr.setPower(pivot + (vertical + horizontal));
+        bl.setPower(pivot + (vertical + horizontal));
+        br.setPower(pivot + (vertical - horizontal));
+
+
     }
 
     public void claw(){
         // Difficulty: MEDIUM
         // Phoenix
         // Assume we are using the same button to open and close the claw
-        double currentPos = clawServo.getPosition(); // var used to track the state of the claw
-        if (gamepad2.a && currentPos == 0.0) {
-            clawServo.setPosition(1.0); // fully open
-        } else {
-            clawServo.setPosition(0.0); // fully closed
-        }
     }
 
     public void lift(){
@@ -50,17 +72,17 @@ public abstract class Telelib extends OpMode {
         // Assume we are using one motor to power the lift
         // Assume we are using the joysticks to control the lift
 
-        if(Math.abs(gamepad2.right_stick_y) > .3) {
-            motorLift.setPower(Math.signum(gamepad2.right_stick_y) * .25);
-        } else {
-            motorLift.setPower(0.0);
-        }
     }
 
     public void kill(){
         // Difficulty: EASY
         // Elin
         // This method shuts off the power from all motors
+        fl.setPower(0);
+        fr.setPower(0);
+        bl.setPower(0);
+        br.setPower(0);
+
     }
 
 }
